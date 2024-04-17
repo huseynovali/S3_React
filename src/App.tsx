517ed39d5e5d20bdb8e4578de2d0ed2108eee11b
User <huseynovali612@gmail.com>
2024-04-17 22:26:14 +0400

    create product etail page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import MainLayout from "./layout/MainLayout";
 import Home from "./page/Home";
+import ProductDetail from "./page/ProductDetail";
+
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="dashboard" element={<a>dashboard</a>} />
+        <Route path="/:id" element={<ProductDetail/>} />
         <Route path="*" element={<a>sdasda</a>} />
       </Route>
     </Routes>
---
+++ src/page/ProductDetail.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductDetailsComp from '../components/ProductDetailsComp'
+
+function ProductDetail() {
+  return (
+    <div><ProductDetailsComp/></div>
+  )
+}
+
+export default ProductDetail
