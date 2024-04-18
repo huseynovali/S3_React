ce6532b506aa06f695b7f932f878cf9e4a980f13
User <huseynovali612@gmail.com>
2024-04-18 05:12:08 +0400

    create admin product list

--- src/components/Admin/AdminProductList.tsx
+++ src/components/Admin/AdminProductList.tsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
+const products = [
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+];
 function AdminProductList() {
   return (
-    <div>AdminProductList</div>
-  )
+    <div className="bg-white  overflow-hidden ">
+      <ul className=" ">
+        {products.map((product) => (
+          <li key={product.name} className="my-2 border-2">
+            <a href={product.href} className="block hover:bg-gray-50">
+              <div className="flex items-center px-4 py-4 sm:px-6">
+                <div className="min-w-0 flex-1 flex items-center">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="h-12 w-12 rounded-full"
+                      src={product.imageSrc}
+                      alt={product.imageAlt}
+                    />
+                  </div>
+                  <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
+                    <div>
+                      <p className="text-sm font-medium text-indigo-600 truncate">
+                        {product.name}
+                      </p>
+                      <p className="mt-2 flex items-center text-sm text-gray-500">
+                        <span className="truncate">{product.price}</span>
+                      </p>
+                    </div>
+                    <div className="hidden md:block"></div>
+                  </div>
+                </div>
+                <div>
+                  <ChevronRightIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+              </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default AdminProductList
+export default AdminProductList;
